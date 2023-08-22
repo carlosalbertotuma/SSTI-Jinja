be50681128a73e64f6cdb8a65eecb3809ef9e162
Bl4dSc4n <carlosalbertotuma@yahoo.com.br>
2023-08-22 16:56:32 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    output = None
+    if request.method == 'POST':
+        codigo = request.form['codigo']
+        try:
+            exec_result = eval(codigo)
+            output = repr(exec_result)
+        except Exception as e:
+            output = f"Erro: {str(e)}"
+    return render_template('index.html', output=output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
