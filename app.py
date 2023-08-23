b24bc928193e7c4fa682412a3bcd665bbbbcf147
Bl4dSc4n <carlosalbertotuma@yahoo.com.br>
2023-08-23 11:42:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.static_folder = 'static'
-
+app.static_folder = 'templates'
+app.config['MAX_CONTENT_LENGTH'] = None
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -11,7 +12,7 @@ def index():
         codigo = request.form['codigo']
         try:
             exec_result = eval(codigo)
-            output = repr(exec_result)
+            output = str(exec_result)
         except Exception as e:
             output = f"Erro: {str(e)}"
     return render_template('index.html', output=output)
